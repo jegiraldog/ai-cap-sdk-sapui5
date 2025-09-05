2fc0d54efa6699d8df1f27565f73de9d7d6b8709
Jhony E. Giraldo <jhony.giraldo@globant.com>
2025-09-05 14:39:40 +0000

    Initial commit: AI Chat PDF UI5 application

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.globant.aichat.aichatpdfui5.controller.Home", {
+        onInit() {
+        }
+    });
+});
